5a604dc30ed483ef2c8dea29710f648d6ee38772
VTramon <vitorr.ooliveira@gmail.com>
2022-01-03 17:01:21 -0300

    first commit

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,20 @@
+const SideBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Luffy</li>
+        <li>Zoro</li>
+        <li>Nami</li>
+        <li>Usopp</li>
+        <li>Sanji</li>
+        <li>Chopper</li>
+        <li>Robin</li>
+        <li>Franky</li>
+        <li>Brook</li>
+        <li>Jinbe</li>
+      </ul>
+    </div>
+  );
+};
+
+export { SideBar };
